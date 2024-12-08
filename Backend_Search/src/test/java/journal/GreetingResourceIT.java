9e3e5c38c6ae495f1d3a2dfd5935a8fafa95e402
Olivia Hultman <olivoljahultman@gmail.com>
2024-12-08 01:14:15 +0100

    Tagit bort tester

--- Backend_Search/src/test/java/journal/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package journal;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
